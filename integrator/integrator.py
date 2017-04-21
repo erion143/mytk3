7d768f07ce3df91450740ee9c55c3e6ade3250cc
erion143 <erion143@yandex.ru>
2017-04-21 11:49:21 +0300

    moving some functions

--- integrator/integrator.py
+++ integrator/integrator.py
@@ -10,35 +10,7 @@ sys.path.append('..')
 from graph3 import Deckart3, Line3
 from mytk3 import TextWall
 from myparser import parser
-from tlib import make_weight
-
-
-def integrator(xs, ys, x_min=None, x_max=None):
-    # Это писец, товарици. Как бы сделать по красивее?
-    if x_min is not None:
-        foo = [x >= x_min for x in xs]
-    else:
-        foo = []
-    if x_max is not None:
-        bar = [x <= x_max for x in xs]
-    else:
-        bar = []
-    if foo and bar:
-        res = [lx and rx for lx, rx in zip(foo, bar)]
-        xs = [x for x, lx in zip(xs, res) if lx]
-        ys = [y for y, lx in zip(ys, res) if lx]
-    elif foo:
-        xs = [x for x, lx in zip(xs, foo) if lx]
-        ys = [y for y, lx in zip(ys, foo) if lx]
-    elif bar:
-        xs = [x for x, lx in zip(xs, bar) if lx]
-        ys = [y for y, lx in zip(ys, bar) if lx]
-
-    summ = 0
-    for (x1, y1), (x2, y2) in zip(zip(xs, ys), zip(xs[1:], ys[1:])):
-        summ += ((y1 + y2) / 2 * (x2 - x1))
-
-    return summ
+from tlib import make_weight, integrator
 
 
 class Container:
--- integrator/tlib.py
+++ integrator/tlib.py
@@ -14,3 +14,49 @@ def make_weight(source, *rules):
             res.append(start)
 
     return res
+
+
+def integrator(xs, ys, x_min=None, x_max=None):
+    # Это писец, товарици. Как бы сделать по красивее?
+    if x_min is not None:
+        foo = [x >= x_min for x in xs]
+    else:
+        foo = []
+    if x_max is not None:
+        bar = [x <= x_max for x in xs]
+    else:
+        bar = []
+    if foo and bar:
+        res = [lx and rx for lx, rx in zip(foo, bar)]
+        xs = [x for x, lx in zip(xs, res) if lx]
+        ys = [y for y, lx in zip(ys, res) if lx]
+    elif foo:
+        xs = [x for x, lx in zip(xs, foo) if lx]
+        ys = [y for y, lx in zip(ys, foo) if lx]
+    elif bar:
+        xs = [x for x, lx in zip(xs, bar) if lx]
+        ys = [y for y, lx in zip(ys, bar) if lx]
+
+    summ = 0
+    for (x1, y1), (x2, y2) in zip(zip(xs, ys), zip(xs[1:], ys[1:])):
+        summ += ((y1 + y2) / 2 * (x2 - x1))
+
+    return summ
+
+
+def intervals(xs):
+    for x1, x2 in zip(xs, xs[1:]):
+        yield x2 - x1
+
+
+def speed(ts, vs):
+    volumes = []
+    speeds = []
+
+    for v, dt, dv in zip(vs, intervals(ts), intervals(vs)):
+        volumes.append(v + dv // 2)
+        speeds.append(round(dv / dt, 2))
+
+    return volumes, speeds
+
+
